4c303e271c336da207d2cef8aff57f5ed0337b4a
christiantigor <christian.tigor@rocketmail.com>
2014-07-22 12:29:15 +0700

    Initial commit
    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rpmAppApp
+ * @description
+ * # rpmAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('rpmAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rpmAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rpmAppApp
+ */
+angular.module('rpmAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
